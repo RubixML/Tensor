2c5d48498acdc7185819060045c8dc26cd15dd40
Michele Locati <michele@locati.it>
2022-04-03 05:16:04 +0200

    Make it so tests fail if PHP outputs warnings at startup (#29)

--- .github/workflows/ci-ext.sh
+++ .github/workflows/ci-ext.sh
@@ -24,3 +24,10 @@ pecl package /app/package.xml
 MAKE="make -j$(nproc)" pecl install tensor-*.tgz
 docker-php-ext-enable tensor
 php --ri tensor
+
+# Let's check that PHP doesn't output any warning
+TENSOR_PHP_OUTPUT="$(php -r ';' 2>&1)"
+if [ -n "$TENSOR_PHP_OUTPUT" ]; then
+  printf 'PHP displayed these warnings at startup:\n%s\n' "$TENSOR_PHP_OUTPUT" >&2
+  exit 1
+fi
