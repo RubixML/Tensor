e82875e74469b19dfaf445e6eb822c136d90e2c9
myfluxi <mvdw@arcor.de>
2023-04-07 21:45:54 +0200

    Update prototypes in extension code
    Fixes a few deprecation warnings such as
    
      Return type of Tensor\Vector::getIterator() should either be compatible with
      IteratorAggregate::getIterator(): Traversable, or the #[\ReturnTypeWillChange] attribute should be
      used to temporarily suppress the notice in Unknown on line 0

--- ext/tensor/matrix.zep.h
+++ ext/tensor/matrix.zep.h
@@ -731,7 +731,11 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_tensor_matrix_offsetget, 0, 1, I
 	ZEND_ARG_INFO(0, index)
 ZEND_END_ARG_INFO()
 
+#if PHP_VERSION_ID >= 80000
+ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(arginfo_tensor_matrix_getiterator, 0, 0, Traversable, 0)
+#else
 ZEND_BEGIN_ARG_INFO_EX(arginfo_tensor_matrix_getiterator, 0, 0, 0)
+#endif
 ZEND_END_ARG_INFO()
 
 ZEPHIR_INIT_FUNCS(tensor_matrix_method_entry) {
